df6865e6935d21e9fc80411a9e16bc46957d4bec
Magdalena <magdalena.r.stevens@gmail.com>
2017-05-18 23:02:54 +0200

    Updated and added proiects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -31,6 +31,89 @@ var bio = {
         }
 };
 
+
+//EDU
+
+var education = {
+    "schools" : [
+    {
+        "name" : "CRACOW SCHOOL OF ART AND FASHION DESIGN",
+        "location" : "Cracow, Poland",
+        "degree" : "Fashion Designer Diploma",
+        "majors" : "Artistic Fashion Design, Jewelry",
+        "dates" : "2010 - 2014"
+        // "url" : "http://www.ksa.edu.pl/fashion-college-english"
+    },
+        {
+        "name" : "The Social High School of United Europe",
+        "location" : "Zory, Poland",
+        "degree" : "High School Diploma and Matura",
+        "majors" : "English, Polish Literature, Maths",
+        "dates" : "2007 - 2010"
+        // "url" : "http://www.szkoladlawas.pl/liceum"
+    }
+    ],
+    "onlineCourses" : [
+    {
+        "title" : "Front-End Web Developer Nanodegree Program",
+        "school" : "Udacity",
+        "date" : "2017",
+        "url" : "https://www.udacity.com/"
+    },
+    {
+        "title" : "CSS Basics & CSS Selectors",
+        "school" : "Treehouse",
+        "date" : "2017",
+        "url" : "https://teamtreehouse.com/home"
+    },
+    {
+        "title" : "JavaScript Basics",
+        "school" : "Treehouse",
+        "date" : "2017",
+        "url" : "https://teamtreehouse.com/home"
+    },
+    {
+        "title" : "WordPress Basics & User Roles",
+        "school" : "Treehouse",
+        "date" : "2017",
+        "url" : "https://teamtreehouse.com/home"
+    }
+    ]
+}
+
+
+education.schools.forEach(function(school, index) {
+    $('#education').append(HTMLschoolStart);
+    var formattedSchoolName = HTMLschoolName.replace("%data%", school.name);
+    var formattedSchoolDates = HTMLschoolDates.replace("%data%", school.dates);
+    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
+    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
+    var formattedSchoolMajors = HTMLschoolMajor.replace("%data%", school.majors);
+    $('.education-entry:last').append(formattedSchoolName);
+    $('.education-entry:last').append(formattedSchoolDates);
+    $('.education-entry:last').append(formattedSchoolLocation);
+    $('.education-entry:last').append(formattedSchoolDegree);
+    $('.education-entry:last').append(formattedSchoolMajors);
+
+
+});
+
+$('#education').append(HTMLonlineClasses);
+education.onlineCourses.forEach(function(online, index) {
+    $('#education').append(HTMLschoolStart);
+    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", online.title);
+    var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", online.school);
+    var formattedOnlineDates = HTMLonlineDates.replace("%data%", online.date);
+    var formattedOnlineURL = HTMLonlineURL.replace("%data%", online.url);
+    $('.education-entry:last').append(formattedOnlineTitle);
+    $('.education-entry:last').append(formattedOnlineSchool);
+    $('.education-entry:last').append(formattedOnlineDates);
+    $('.education-entry:last').append(formattedOnlineURL);
+
+
+
+});
+
 // WORK
 
 var work = {
@@ -90,47 +173,36 @@ work.display = function () {
 
 work.display();
 
+//PROJECTS
 
+   var projects = {
+      "projects" : [
+      {
+         "title": "Online Resume",
+         "dates": "May 2017",
+         "description": "" + "Technologies used: HTML, CSS, JS, Bootstrap.",
+         "images": ["images/online_portfolio.png"]
+     },
+     {
+         "title": "Animal Trading Card",
+         "dates": "April 2017",
+         "description": "First CSS project using HTML and CSS.",
+         "images": ["images/animal_trading_card.png"]
+     }]
+ };
 
-//EDU
+ projects.display = function() {
+     projects.projects.forEach(function(project, index) {
+         $("#projects").append(HTMLprojectStart);
 
-var education = {
-    "schools" : [
-    {
-        "name" : "CRACOW SCHOOL OF ART AND FASHION DESIGN",
-        "location" : "Cracow, Poland",
-        "degree" : "Fashion Designer Diploma",
-        "majors" : "Artistic Fashion Design, Jewelry",
-        "dates" : "2010 - 2014"
-        // "url" : "http://www.ksa.edu.pl/fashion-college-english"
-    },
-        {
-        "name" : "The Social High School of United Europe",
-        "location" : "Zory, Poland",
-        "degree" : "High School Diploma and Matura",
-        "majors" : "English, Polish Literature, Maths",
-        "dates" : "2007 - 2010"
-        // "url" : "http://www.szkoladlawas.pl/liceum"
-    }
-    ]
-}
+         $(".project-entry:last").append(HTMLprojectTitle.replace("%data%", project.title));
+         $(".project-entry:last").append(HTMLprojectDates.replace("%data%", project.dates));
+         $(".project-entry:last").append(HTMLprojectDescription.replace("%data%", project.description));
+         $(".project-entry:last").append(HTMLprojectImage.replace("%data%", project.images));
+     });
+ };
 
-education.schools.forEach(function(school, index) {
-    $('#education').append(HTMLschoolStart);
-    var formattedSchoolName = HTMLschoolName.replace("%data%", school.name);
-    var formattedSchoolDates = HTMLschoolDates.replace("%data%", school.dates);
-    var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", school.location);
-    var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", school.degree);
-    var formattedSchoolMajors = HTMLschoolMajor.replace("%data%", school.majors);
-    $('.education-entry:last').append(formattedSchoolName);
-    $('.education-entry:last').append(formattedSchoolDates);
-    $('.education-entry:last').append(formattedSchoolLocation);
-    $('.education-entry:last').append(formattedSchoolDegree);
-    $('.education-entry:last').append(formattedSchoolMajors);
-
-
-});
-
+projects.display();
 
 // BIO
 var name = bio.name;
@@ -173,6 +245,7 @@ if (bio.skills.length > 0) {
      bio.skills.forEach (function(skill, index) {
         var formattedSkill = HTMLskills.replace("%data%", skill);
         $('#skills').append(formattedSkill);
-})};
+});
+};
 
 $('#mapDiv').append(googleMap);
